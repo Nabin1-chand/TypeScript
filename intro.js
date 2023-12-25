d2be260223546de6b2dd1a8511aaa3b8e79d04b7
Nabin12chand <nabin.async.chand@gmail.com>
2023-12-25 11:18:48 +0545

    typescript example

---
+++ intro.js
@@ -0,0 +1,3 @@
+console.log('Hello Nabin welcome to js world');
+var user = { name: "Nabin", age: 24 };
+console.log(user.name);
---
+++ intro.ts
@@ -0,0 +1,4 @@
+console.log('Hello Nabin welcome to js world');
+const user  ={name :"Nabin", age:24};
+
+console.log(user.name); 
